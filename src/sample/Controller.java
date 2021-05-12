a57eb6a1363948b2c52882aea1dbfef196242f6d
Elena <l271971_88@mail.ru>
2021-05-12 12:10:47 +0300

    Сериализация

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Controller {
     Socket socket;
